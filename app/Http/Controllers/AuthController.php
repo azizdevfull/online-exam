be520fea39f1fec02ea9e7782661cea11744b529
azizdevfull <aziz16110904@gmail.com>
2022-09-27 10:50:37 +0500

    Show Exams on Student Dashboard

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Exam;
 use App\Models\Subject;
 use Illuminate\Support\Facades\Hash;
 
@@ -82,7 +83,8 @@ class AuthController extends Controller
 
     public function loadDashboard()
     {
-        return view('student.dashboard');
+        $exams = Exam::with('subjects')->orderBy('date')->get();
+        return view('student.dashboard',['exams' => $exams]);
     }
 
 
--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -1,4 +1,41 @@
 @extends('layout/student-layout')
 @section('space-work')
-    
+    <h2>Exams</h2>
+
+    <table class="table">
+
+        <thead>
+            <th>#</th>
+            <th>Exam Name</th>
+            <th>Subject Name</th>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Total Attempt</th>
+            <th>Available Attempt</th>
+            <th>Copy Link</th>
+        </thead>
+
+        <tbody>
+            @if (count($exams) > 0)
+            @php $count = 1;  @endphp
+                @foreach ($exams as $exam)
+                    <tr>
+                        <td>{{ $count++ }}</td>
+                        <td>{{ $exam->exam_name }}</td>
+                        <td>{{ $exam->subjects[0]['subject']}}</td>
+                        <td>{{ $exam->date }}</td>
+                        <td>{{ $exam->time }} Hrs</td>
+                        <td>{{ $exam->attempt }} Time</td>
+                        <td>{{ $count++ }}</td>
+                    </tr>
+                @endforeach
+            @else
+                <tr>
+                    <td colspan="8">Not Exams Available!</td>
+                </tr>
+            @endif
+        </tbody>
+
+    </table>
+
 @endsection
