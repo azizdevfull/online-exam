a9414cb407992f8c493c8823439a80eba876590d
azizdevfull <aziz16110904@gmail.com>
2022-09-28 14:09:48 +0500

    Exam Page set up with Full Validation

--- resources/views/student/exam-dashboard.blade.php
+++ resources/views/student/exam-dashboard.blade.php
@@ -5,20 +5,20 @@
     <div class="container">
         <p style="color:black;">Welcome, {{ Auth::user()->name }}</p>
         <h1 class="text-center">{{ $exam[0]['exam_name'] }}</h1>
+        @php  $qcount = 1;  @endphp
         @if ($success == true)
             @if (count($qna) > 0)
-            <form action="" method="POST" class="mb-5">
+            <form action="" method="POST" class="mb-5" onsubmit="return isValid()">
                 <input type="hidden" name="exam_id" value="{{ $exam[0]['id'] }}">
-            @php  $qcount = 1;  @endphp
                 @foreach ($qna as $data)
                     <div>
                         <h5>Q{{ $qcount++ }}. {{ $data['question'][0]['question'] }}</h5>
                         <input type="hidden" name="q[]" value="{{ $data['question'][0]['id'] }}" >
-                        <input type="text" name="ans_{{ $qcount-1 }}">
+                        <input type="hidden" name="ans_{{ $qcount-1 }}" id="ans_{{ $qcount-1 }}">
                         @php  $acount = 1;  @endphp
                         @foreach ($data['question'][0]['answers'] as $answer)
                             <p><b>{{ $acount++ }}) </b>{{ $answer['answer'] }}
-                                <input type="radio" name="radio_{{ $qcount-1 }}" class="select_ans" value="{{ $answer['id'] }}" >        
+                                <input type="radio" name="radio_{{ $qcount-1 }}" data-id="{{ $qcount-1 }}" class="select_ans" value="{{ $answer['id'] }}" >        
                         </p>
                         @endforeach
                     </div>
@@ -35,5 +35,45 @@
             <h3 style="color:red;" class="text-center">{{ $msg }}</h3>
         @endif
     </div>
+
+
+
+
+    <script>
+        $(document).ready(function () {
+
+            $(".select_ans").click(function() {
+                var no = $(this).attr("data-id");
+                $('#ans_'+no).val($(this).val());
+            });
+
+
+        });
+
+        function isValid(){
+                var result = true;
+
+                var qlength = parseInt("{{$qcount}}")-1;
+                $('.error_msg').remove();
+                for (let i = 1; i <= qlength; i++) {
+                   
+                    if ($('#ans_'+i).val() == "") {
+                        result = false;
+                        $('#ans_'+i).parent().append('<span style="color:red;" class="error_msg">Please select answer.</span>');
+                        
+                        setTimeout(() => {
+                            $('.error_msg').remove();
+                        }, 5000);
+
+                    }
+                    
+                }
+
+                return result;   
+            }
+    </script>
+
+
+
 
 @endsection
